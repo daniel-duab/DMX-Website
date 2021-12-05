e0df10c7d632b6fac94dda7d7588aeb593466233
hrtze <piduartebaird@gmail.com>
2021-12-04 19:55:00 -0500

    buch i stf

--- index.js
+++ index.js
@@ -1,185 +1,98 @@
-const app = require('express')();
-
-const i2c = require("i2c-bus");
-let bus = i2c.open(0x50, (e)=>{console.log(e)});
-
+const { error } = require('console');
+const express = require('express');
+const { json } = require('express/lib/response');
+const app = express();
 
 const http = require('http').Server(app); 
 
 const io = require('socket.io')(http); 
 
-const { lookupService } = require('dns'); 
-
 const fs = require('fs'); 
+const { Socket } = require('socket.io-client');
 
-const portus = process.env.PORT || 25656; 
+const portus = process.env.PORT || 3000; 
 
-//const SerialPort = require("serialport"); 
-//var arduino = new SerialPort("/dev/cu.usbserial-14140", 9600);
+var arduino;
 
-//let Readline = SerialPort.parsers.Readline; 
-//let parser = new Readline(); 
-//arduino.pipe(parser); 
+/*
+  const SerialPort = require("serialport");
+  arduino = new SerialPort("/dev/cu.usbserial-14140", 9600);
 
-let ids = [];
-let ons = [];
-let gets = [];
-let sliders = [
-["Jake", [1], 0], 
-["Finn", [1], 0], 
-["GrassWizard", [1], 0], 
-["BMO", [1], 0], 
-["Default", [1, 2, 3, 4], 0]
-];
+  let Readline = Serial Port.parsers.Readline; 
+  let parser = new Readline(); 
+  arduino.pipe(parser); 
+
+  arduino.on("open", () => {
+      console.log('serial port open');
+  });
+*/
 
 
-//arduino.write("1c100v")
+app.use(express.static(__dirname + "/public"))
+
+
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
   });
 
-  function clons(){
-    for(i=0; i<512; i++){
-        gets[i] = [false, -1];
-        ons[i] = false;
-    }
-  }
-function start(){
-    for(i=0; i<512; i++){
-        ons.push(false);
-        gets.push([false, -1]);
-    }
-    console.log('Ons Created!');
-    console.log('gets Created!');
-}
-start();
-  
 
-app.get('/new', (req, res) => {
-    res.sendFile(__dirname + '/newCues.html');
-  });
-  app.get('/del', (req, res) => {
-    res.sendFile(__dirname + '/delSliders.html');
-  });
-  app.get('/btns', (req, res) => {
-    res.sendFile(__dirname + '/btns.html');
-  });
-
-  /*arduino.on("open", () => {
-    console.log('serial port open');
-  });
-  parser.on('data', data=>{
-      //console.log(data);
-  })*/
 
 
 io.on('connection', (socket) =>{
-    socket.emit('id', socket.id)
-    socket.emit('sliders', sliders);
-    ids.push(socket.id);
-    socket.on('est', (id)=>{
-        console.log(id + ' CONNECTED');
-    });
+  console.log(socket.id)
+    socket.on('scene-change', (obje)=>{
+      console.log('intop')
+      fs.readFile('./db/scene.json', (err, data)=>{
+        //get the data
+        try{
+        let update = JSON.parse(data);
+        //nav to actual scene
+        //console.log(update.object)
+        // iterate through childrem
+        for (let i = 0; i<update.object.children.length; i++){
+          // set the child val to the updated sent val
+            let childName = update.object.children[i].name;
+            console.log('cn' + childName + JSON.stringify(obje))
+            if (childName === obje.name){
+              update.object.children[i] = obje;
+              fs.writeFile('./db/scene.json', update, 'utf-8', (err)=>{});
+              socket.emit('scene-update', (update))
+              console.log('success ' + update)
+            }
+            //write it back to json file
+            
+          }
+        }catch (errer){
 
-    socket.on('estMK', (id)=>{
-        console.log(id + ' CONNECTED MK');
+          socket.emit('request-full-scene')
+          console.log('err in chang ' + errer)
+        }
+      })
+    })
+    socket.on('full-scene', (scene)=>{
+      fs.writeFile('./db/scene.json', JSON.stringify(scene), 'utf-8', (err)=>{console.log('fs')});
+      socket.emit('scene-update', JSON.stringify(scene))
     });
-
-socket.on('pause', ()=>{
-    io.emit('stop');
+    socket.on('scene-get', ()=>{
+      fs.readFile('./db/scene.json', (err, data)=>{
+        try{
+          socket.emit('scene-return', JSON.parse(data))
+        }catch{
+          socket.emit('fuck! an error happened!')
+        }
+        
+      });
+    })
 });
 
-    socket.on('disconnect', ()=>{
-        //console.log(socket.id + " DISCONNNECTED");
-        for (i=0; i<ids.length; i++) {
-            if (ids[i] == socket.id){
-                ids.splice(i, 0);
-            }
-        }
-    });
 
-    socket.on('newSlider', (newSlider)=>{
-        
-        sliders.unshift(newSlider);
-        io.emit('sliders', sliders);
-        //sendallBytes();
-        
-    });
-    
-    socket.on('updateSlider', (l)=>{
-            if(l[0]<sliders.length){
-            console.log("n: " + l[0] + "   v: " + l[1] + "  id: " + l[2])
-            sliders[l[0]][2] = l[1]
-            io.emit('nsliders', sliders, l[2]);
-            bus.i2cWrite("HELLO YO :)")
-            //sendallBytes();
-        }
-        
-        
-    });
-//socket.on('newBytes', sendallBytes);
-    socket.on('delSlider', (data)=>{
-            for(i=0; i<sliders[data][1].length; i++){
-                var sendDat = sliders[data][1][i] + 'c0v'
-            }
-            sliders.splice(data, 1);
-            //console.log('deleted slider #' + data);
-            io.emit('sliders', sliders);
-            
-        })
-    });
-    /* THIS FUNCTION NEEDS TO BE REFACTORED FOR I2C INSTEAD OF THE UART SERIAl I WAS USING BEFORE!!!
+function sendByte(byte){
+    arduino.write(byte);
+}
 
-    function sendallBytes(){
-        
-        for(i=0; i<sliders.length; i++){
-            for(e=0; e<sliders[i][1].length; e++){
-                if (sliders[i][2] > 0){
-                    gets[sliders[i][1][e]] = [true];
-                }
-                
-            }
-            for(e=0; e<sliders[i][1].length; e++){
-                
-                    //console.log('a');
-                    //if(ons[sliders[i][1][e]] === false){
-                    if (ons[sliders[i][1][e]] === false){
-                        console.log(sliders)
-                        
-                        var bite = sliders[i][1][e] + 'c' + sliders[i][2] + 'v';
-                        //arduino.write(bite);
-                        
-                        console.log(bite);
-                        //console.log(sliders[i][2])
-                        
-                        if(gets[sliders[i][1][e]][0]){
-                            ons[sliders[i][1][e]] = true;
-                            arduino.write(bite);
-                        }else{
-                            arduino.write(sliders[i][1][e] + 'c0v')
-                        }
-                        
-                    }else{console.log('written')}
-                    }
-            //}else{console.log('no ' + ri)}
-        }
-        clons()
-            
-
-        }*/
-        /*
-        function ser(dat){
-            arduino.write(dat)
-        }
-        function sendByte(bite){
-            arduino.write(bite);
-        }
-*/
 
 
 http.listen(portus, () => {
     console.log(`server running at http://localhost:${portus}/`);
   });
-
-
