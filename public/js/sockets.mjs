dd2ad1b590d09d009ce7dedf225e9f2e9dbc724a
hrtze <piduartebaird@gmail.com>
2021-12-09 19:44:09 -0500

    deleted js, uneede for this branch

--- public/js/sockets.mjs
+++
@@ -1,8 +0,0 @@
-import { scene } from "./canvas.mjs";
-import { ObjectLoader } from "./vendor/three/build/three.module.js";
-
-//import { io } from "socket.io-client/dist/socket.io"
-let socket = io();
-
-
-export {socket}
