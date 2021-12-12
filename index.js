b56625debc994ce278271fc5605872dd0f95fa96
hrtze <piduartebaird@gmail.com>
2021-12-11 22:53:00 -0500

    it looks pretty cool now, working on button functionallity

--- index.js
+++ index.js
@@ -38,67 +38,6 @@ app.get('/', (req, res) => {
 
 
 
-io.on('connection', (socket) =>{
-  fs.readFile('./db/scene.json', (err, data)=>{
-    try{
-      socket.emit('scene-return', JSON.parse(data))
-    }catch{
-      socket.emit('fuck! an error happened!')
-    }
-  });
-  console.log(socket.id)
-    socket.on('scene-change', (obje)=>{
-      fs.readFile('./db/scene.json', 'utf8', (err, data)=>{
-        //get the data
-        try{
-        let update = JSON.parse(data);
-        //nav to actual scene
-
-        // iterate through childrem
-        let ifNew = true;
-        for (let i = 0; i<update.object.children.length; i++){
-          // set the child val to the updated sent val
-            let childName = update.object.children[i].name;
-            if (childName === obje.object.name){
-              update.object.children[i] = obje;
-              fs.writeFile('./db/scene.json', JSON.stringify(update), 'utf-8', (err)=>{console.log('oobj update')});
-              socket.broadcast.emit('scene-update', (update))
-              
-              ifNew = false
-            }else if(ifNew){
-              update.object.children[i] = obje;
-              
-              fs.writeFile('./db/scene.json', JSON.stringify(update), 'utf-8', (err)=>{console.log('nobj update')});
-              socket.broadcast.emit('scene-add', (update))
-              
-              
-            }
-            //write it back to json file
-            
-          }
-        }catch (errer){
-
-          socket.emit('request-full-scene')
-          console.error(errer);
-        }
-      })
-    })
-    socket.on('full-scene', (scene)=>{
-      fs.writeFile('./db/scene.json', JSON.stringify(scene), 'utf-8', (err)=>{console.log('fs')});
-      io.emit('scene-update', JSON.stringify(scene))
-    });
-    socket.on('scene-get', ()=>{
-      fs.readFile('./db/scene.json', (err, data)=>{
-        try{
-          socket.emit('scene-return', JSON.parse(data))
-        }catch{
-          socket.emit('fuck! an error happened!')
-        }
-      });
-    });
-});
-
-
 function sendByte(byte){
     arduino.write(byte);
 }
---
+++ public/js/otros.js
@@ -0,0 +1,33 @@
+
+
+function fold(element, button){
+    //let elem = document.getElementById("ee")
+    let elementData = JSON.parse(element.getElementsByClassName("data").item(0).innerHTML)
+    
+    
+    if (button.innerHTML == "▶"){
+        button.innerHTML = "▼"
+    }else{
+        button.innerHTML = "▶"
+    }
+    if(elementData.folded == false){
+        elementData.folded = true
+        element.getElementsByClassName("data").item(0).innerHTML = JSON.stringify(elementData)
+        for(tohide of element.childNodes){
+            if (tohide.className == "panel-header"){
+                
+            }else{
+                tohide.hidden=true;
+            }
+        }
+    }else{
+        elementData.folded = false;
+        element.getElementsByClassName("data").item(0).innerHTML = JSON.stringify(elementData)
+        for(tohide of element.childNodes){
+            if(tohide.className != "data")
+                tohide.hidden = false
+        }
+
+    }
+
+}
