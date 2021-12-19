139d550356ee9e845f093f8723cea475c38f83ec
hrtze <piduartebaird@gmail.com>
2021-12-19 18:02:54 -0500

    finishing up and moving back to the old three js

---
+++ public/js/sliderFormatting.js
@@ -0,0 +1,6 @@
+function leSlidé(){
+    // for(slider in document.getElementsByClassName("light-slider").item(0).getAttribute("style")){
+        console.log(document.getElementsByClassName("light-slider").item(0).getAttribute("style"))
+    // }
+}
+setInterval(leSlidé, 10000)
